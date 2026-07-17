9b96dcf9d9f3c7b0ff3dce6fe4c579e80e39d637
param20h <paramjitsingh.23@lpu.in>
2026-07-17 18:18:30 +0530

    chore: change Next.js output config to standalone for Cloudflare Workers deploy

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  output: "export",
-  images: {
-    unoptimized: true, // Required for static exports in Next.js
-  },
+  output: "standalone",
 };
 
 export default nextConfig;
